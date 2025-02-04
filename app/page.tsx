f7b17f517410141d112ecc85b60753d099fb9f42
Griffin Hobbs <ffirgn@gmail.com>
2025-02-04 10:15:18 -0800

    updated hero page for sizing

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <div className="absolute top-0 z-0 h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-        <Hero/>
+        <Hero />
       </div>
     </main>
   );
